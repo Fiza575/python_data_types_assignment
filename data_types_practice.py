08d5d3a4bb78a4dbe98f19ff7fb2112de1c906a4
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-05-05 19:21:40 +0500

    Add files via upload

---
+++ data_types_practice.py
@@ -0,0 +1,25 @@
+# Declare variables of each of the following
+# Integer
+number_of_students = 16
+balance = 0
+print("Number_of_students:",number_of_students)
+print("Balance:",balance)
+
+# String
+favorite_color ='Black'
+country = "Hong Kong"
+print("Favorite_color:",favorite_color)
+print("Country:",country)
+
+# Float
+price_in_item = 49.99
+height = 5.6
+print("Price_in_item:",price_in_item)
+print("Height:",height)
+
+# Boolean
+is_student =False
+likes_books = True
+print("Is_student:",is_student)
+print("Likes_books:",likes_books)
+
